e6571d4da3e505d3ff1cfd9d7c74d1590f4f8517
zhutao <zhutao@xcloud.cc>
2018-12-11 15:12:51 +0800

    修改数据库密码

--- sql/00-init-db.sql
+++ sql/00-init-db.sql
@@ -6,6 +6,6 @@ DELETE FROM mysql.user WHERE user='xiaozhao';
 DROP DATABASE xiaozhao;
 
 CREATE DATABASE xiaozhao DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE user 'xiaozhao'@'localhost' identified by 'zhutao';
-GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON xiaozhao.*  TO 'xiaozhao'@'localhost' IDENTIFIED BY 'zhutao';
+CREATE user 'xiaozhao'@'localhost' identified by 'Yunxia520/';
+GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON xiaozhao.*  TO 'xiaozhao'@'localhost' IDENTIFIED BY 'Yunxia520/';
 FLUSH PRIVILEGES;
